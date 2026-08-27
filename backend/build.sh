f7972b7f895dcbca3f62f6185d427d8c1df49c6c
tadesse <tademan121@gmail.com>
2026-08-27 14:14:41 +0300

    Load project and blog data on Render deployment

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,17 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py migrate --no-input
+
+python manage.py shell -c "
+from projects.models import Project
+from blog.models import BlogPost
+
+if Project.objects.count() == 0 and BlogPost.objects.count() == 0:
+    print('Loading project and blog fixture...')
+    import subprocess
+    subprocess.run(['python', 'manage.py', 'loaddata', 'projects_blog.json'], check=True)
+else:
+    print('Projects or blog posts already exist. Skipping fixture.')
+"
+
 python manage.py collectstatic --no-input
